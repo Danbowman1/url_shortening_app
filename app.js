2f7b62376126ebbc5ef8ba6943317a5e9c43b615
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-07-24 17:36:35 -0400

    shortened Url fixed and added copy functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let url = document.getElementById('url-input')
-const btn = document.querySelector('.shorten-btn')
-let links = document.querySelector('.links')
+const submitBtn = document.getElementById('shorten-btn')
+let links = document.getElementById('links')
 
-btn.addEventListener('click', getValue)
+
+submitBtn.addEventListener('click', getValue)
+
 
 function getValue(e) {
     e.preventDefault()
@@ -12,17 +14,49 @@ function getValue(e) {
     .then(res => {
         return res.json()
     })
-    .then(shortened => {
-        links.innerHTML += `
-    <div class="links-container">   
-        <p class="link">${url.value}</p>
-        <hr />
-        <p class="shorten-link">${shortened.result.full_short_link}</p>
-        <button class="copy-btn">Copy</button>
-    </div>
-    `
-    console.log(shortened.result.full_short_link)
+    .then(data => {
+        let div = document.createElement('div')
+        let originalIp = document.createElement('p')
+        let hr = document.createElement('hr')
+        let shortenedIp = document.createElement('p')
+        let copyBtn = document.createElement('button')
+
+        div.classList.add('links-container')
+        originalIp.classList.add('link')
+        shortenedIp.classList.add('shorten-link')
+        copyBtn.classList.add('copy-btn')
+
+        originalIp.textContent = data.result.original_link
+        shortenedIp.textContent = data.result.full_short_link
+        copyBtn.textContent = "Copy"
+
+        links.insertAdjacentElement('afterbegin', div)
+        div.appendChild(originalIp)
+        div.appendChild(hr)
+        div.appendChild(shortenedIp)
+        div.appendChild(copyBtn)
+
+    
+    function copyLink() {
+        let copied = shortenedIp.textContent
+        console.log(shortenedIp)
+
+        navigator.clipboard.writeText(copied).then(() => {
+            copyBtn.textContent = "Copied!"
+            copyBtn.style.background = 'hsl(257, 27%, 26%)'
+        })
+    }
+
+    copyBtn.addEventListener('click', copyLink)
+    
     })
     
 }
+
+
+
+
+
+
+
 
